30f2df9be6602e54d16c47a88cf08930c00491d1
dnaiel <dog830228@gmail.com>
2017-08-27 19:42:56 +0800

    init

---
+++ src/ExtensionTool/ExtensionTool.Tests/ExtensionAttributeTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using ExtensionTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionTool.Tests
+{
+    public class Tag1 : Attribute
+    {
+        public Tag1(string v)
+        {
+            Value = v;
+        }
+
+        public string Value { get; set; }
+    }
+
+    [Tag1("1234")]
+    public class WithTag { }
+
+    public class NoTag { }
+
+    [TestFixture()]
+    public class ExtensionAttributeTests
+    {
+        [Test()]
+        public void Test_WithTagCurrectValue_True()
+        {
+            string recept = "1234";
+
+            string result = typeof(WithTag).GetAttributeValue((Tag1 tag) => tag.Value);
+
+            Assert.AreEqual(result, recept);
+        }
+
+        [Test()]
+        public void Test_NOTage_True()
+        {
+            string recpt = default(string);
+
+            string result = typeof(NoTag).GetAttributeValue((Tag1 tag) => tag.Value);
+
+            Assert.AreEqual(result, recpt);
+        }
+    }
+}
---
+++ src/ExtensionTool/ExtensionTool/ExteionEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionTool
+{
+    public static class ExteionEnum
+    {
+        public static IEnumerable<string> GetEnumString<TEnum>(this TEnum source) where TEnum : struct, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("TEnum must be enum");
+            }
+            var arrEnum = Enum.GetValues(typeof(TEnum));
+            var val = Convert.ToInt32(source);
+            foreach (var e in arrEnum)
+            {
+                if ((val & (int)e) > 0)
+                {
+                    yield return e.ToString();
+                }
+            }
+        }
+    }
+}
