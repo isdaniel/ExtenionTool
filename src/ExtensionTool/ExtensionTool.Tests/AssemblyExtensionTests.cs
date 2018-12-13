ecaaa48bd3f37e6ae24dc3721b5130a7a6588185
Daniel.Shih <dog830228@gmail.com>
2018-12-13 14:56:14 +0800

    add AssemblyExtension.cs and find class from Assembly

---
+++ src/ExtensionTool/ExtensionTool.Tests/AssemblyExtensionTests.cs
@@ -0,0 +1,68 @@
+﻿using ExtensionTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace ExtensionTool.Tests
+{
+    public interface MyInterface { }
+
+    public abstract class ClassBase { }
+
+    public class ClassA : ClassBase { }
+
+    public class ClassB : ClassBase { }
+
+    [TestFixture]
+    public class AssemblyExtensionTests
+    {
+
+        [Test]
+        public void GetAll_InheritClassBase_GetbyCurrentAssembly_TwoClasses()
+        {
+            AssertClassBase(Assembly.GetExecutingAssembly());
+        }
+
+        [Test]
+        public void GetAll_InheritClassBase_GetbyPath_TwoClasses()
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            AssertClassBase(Assembly.LoadFile(assemblyPath));
+        }
+
+        [Test]
+        public void IsNormalClass_True() {
+
+            Assert.IsTrue(typeof(ClassA).IsNormalClass());
+            Assert.IsFalse(typeof(ClassBase).IsNormalClass());
+            Assert.IsFalse(typeof(MyInterface).IsNormalClass());
+        }
+
+        private void AssertClassBase(Assembly ass) {
+
+            var act = ass.GetInstancesByAssembly<ClassBase>();
+
+            var expect = new List<ClassBase>() {
+                new ClassA(),
+                new ClassB()
+            };
+
+            act.IsInstanceOfAll();
+            Assert.IsTrue(act.Count() == 2);
+        }
+    }
+
+
+    public static class TestLibExtension {
+        public static void IsInstanceOfAll<TType>(this IEnumerable<TType> source) {
+            foreach (var item in source)
+            {
+                Assert.IsInstanceOf<ClassBase>(item);
+            }
+        }
+    }
+}
---
+++ src/ExtensionTool/ExtensionTool/AssemblyExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionTool
+{
+    public static class AssemblyExtension
+    {
+        public static IEnumerable<TResult> GetInstancesByAssembly<TResult>(this Assembly ass)
+        {
+            return ass.GetTypes()
+                    .Where(x => typeof(TResult).IsAssignableFrom(x) && x.IsNormalClass())
+                    .Select(x => Activator.CreateInstance(x))
+                    .Cast<TResult>();
+        }
+
+        public static bool IsNormalClass(this Type type) {
+            return type.IsClass && !type.IsAbstract;
+        }
+
+        //public static IEnumerable<TResult> GetTypesByAssembly<TResult>(this Type type,string path)
+        //{
+        //    return Assembly.LoadFile(path).GetInstances<TResult>();
+        //}
+
+        //private static IEnumerable<TResult> GetInstances<TResult>(this Assembly ass) {
+        //    return
+        //}
+    }
+}
