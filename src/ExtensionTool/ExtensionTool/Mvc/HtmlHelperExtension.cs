fd4ac9b2950fa4dd09c4d741af270a61ca570fbe
Daniel.Shih <dog830228@gmail.com>
2019-01-22 15:03:11 +0800

    Add HtmlHelperExtension.

---
+++ src/ExtensionTool/ExtensionTool/Mvc/HtmlHelperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace ExtensionTool.Mvc
+{
+    public static class HtmlHelperExtension
+    {
+        public static MvcHtmlString TextBoxFor<TModel, TProperty>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression,
+            bool IsReadOnly,
+            object htmlAttributes)
+        {
+            string format = (string)null;
+    
+            IDictionary<string, object> htmlAttributes1 = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+
+            if (IsReadOnly && !htmlAttributes1.ContainsKey("@readonly"))
+                htmlAttributes1.Add("readonly","readonly");
+
+            return InputExtensions.TextBoxFor(htmlHelper, expression, format, htmlAttributes1);
+        }
+    }
+}
